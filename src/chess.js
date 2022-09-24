68d4de2ce5fa3eec76cdd9fe05eb4f1f04b81aa4
Pawan Kumar Regoti <pawan.kumar.regoti@maersk.com>
2022-09-24 08:07:54 +0200

    Update chess.js

--- src/chess.js
+++ src/chess.js
@@ -1,69 +1,69 @@
 //Thanks to Oscar Toledo G. (c)2010
-let B, i, y, u, b, I = [], G = 120, x = 10, z = 15, M = 1e4, l = [5, 3, 4, 6, 2, 4, 3, 5, 1, 1, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9, 9, 9, 13, 11, 12, 14, 10, 12, 11, 13, 0, 99, 0, 306, 297, 495, 846, -1, 0, 1, 2, 2, 1, 0, -1, -1, 1, -10, 10, -11, -9, 9, 11, 10, 20, -9, -11, -10, -20, -21, -19, -12, -8, 8, 12, 19, 21];
-const X = (w, c, h, e, S, s) => {
-    let t, o, L, E, d, O = e, N = -M * M, K = 78 - h << x, p, g, n, m, A, q, r, C, J, a = y ? -x : x;
+let selectedCellNumber, i, y, u, userSelectedCellNumber, I = [], G = 120, x = 10, z = 15, M = 1e4, l = [5, 3, 4, 6, 2, 4, 3, 5, 1, 1, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9, 9, 9, 13, 11, 12, 14, 10, 12, 11, 13, 0, 99, 0, 306, 297, 495, 846, -1, 0, 1, 2, 2, 1, 0, -1, -1, 1, -10, 10, -11, -9, 9, 11, 10, 20, -9, -11, -10, -20, -21, -19, -12, -8, 8, 12, 19, 21];
+const calculateNextMove = (w, c, h, e, S, ply) => {
+    let t, o, L, E, d, O = e, N = -M * M, K = 78 - h << x, cellNumber, g, n, m, A, q, r, C, J, a = y ? -x : x;
     y ^= 8;
     G++;
-    d = w || s && s >= h && X(0, 0, 0, 21, 0, 0) > M;
+    d = w || ply && ply >= h && calculateNextMove(0, 0, 0, 21, 0, 0) > M;
     do {
-        if (o = I[p = O]) {
+        if (o = I[cellNumber = O]) {
             q = o & z ^ y;
             if (q < 7) {
                 A = q-- & 2 ? 8 : 4;
                 C = o - 9 & z ? [53, 47, 61, 51, 47, 47][q] : 57;
                 do {
-                    r = I[p += l[C]];
-                    if (!w | p == w) {
-                        g = q | p + a - S ? 0 : S;
+                    r = I[cellNumber += l[C]];
+                    if (!w | cellNumber == w) {
+                        g = q | cellNumber + a - S ? 0 : S;
                         if (!r & (!!q | A < 3 || !!g) || (r + 1 & z ^ y) > 9 && q | A > 2) {
                             if (m = !(r - 2 & 7))
                                 return y ^= 8,
                                 I[G--] = O,
                                 K;
                             J = n = o & z;
-                            E = I[p - a] & z;
+                            E = I[cellNumber - a] & z;
                             t = q | E - 7 ? n : (n += 2,
                             6 ^ y);
                             while (n <= t) {
                                 L = r ? l[r & 7 | 32] - h - q : 0;
-                                if (s)
-                                    L += (1 - q ? l[(p - p % x) / x + 37] - l[(O - O % x) / x + 37] + l[p % x + 38] * (q ? 1 : 2) - l[O % x + 38] + (o & 16) / 2 : !!m * 9) + (!q ? !(I[p - 1] ^ n) + !(I[p + 1] ^ n) + l[n & 7 | 32] - 99 + !!g * 99 + (A < 2) : 0) + !(E ^ y ^ 9);
-                                if (s > h || 1 < s & s == h && L > z | d) {
-                                    I[p] = n,
+                                if (ply)
+                                    L += (1 - q ? l[(cellNumber - cellNumber % x) / x + 37] - l[(O - O % x) / x + 37] + l[cellNumber % x + 38] * (q ? 1 : 2) - l[O % x + 38] + (o & 16) / 2 : !!m * 9) + (!q ? !(I[cellNumber - 1] ^ n) + !(I[cellNumber + 1] ^ n) + l[n & 7 | 32] - 99 + !!g * 99 + (A < 2) : 0) + !(E ^ y ^ 9);
+                                if (ply > h || 1 < ply & ply == h && L > z | d) {
+                                    I[cellNumber] = n,
                                     I[O] = m ? (I[g] = I[m],
                                     I[m] = 0) : g ? I[g] = 0 : 0;
-                                    L -= X(s > h | d ? 0 : p, L - N, h + 1, I[G + 1], J = q | A > 1 ? 0 : p, s);
-                                    if (!(h || s - 1 | B - O | i - n | p - b | L < -M))
-                                        return W(),
+                                    L -= calculateNextMove(ply > h | d ? 0 : cellNumber, L - N, h + 1, I[G + 1], J = q | A > 1 ? 0 : cellNumber, ply);
+                                    if (!(h || ply - 1 | selectedCellNumber - O | i - n | cellNumber - userSelectedCellNumber | L < -M))
+                                        return drawBoard(),
                                         G--,
                                         u = J;
-                                    J = q - 1 | A < 7 || m || !s | d | r | o < z || X(0, 0, 0, 21, 0, 0) > M;
+                                    J = q - 1 | A < 7 || m || !ply | d | r | o < z || calculateNextMove(0, 0, 0, 21, 0, 0) > M;
                                     I[O] = o;
-                                    I[p] = r;
+                                    I[cellNumber] = r;
                                     m ? (I[m] = I[g],
                                     I[g] = 0) : g ? I[g] = 9 ^ y : 0;
                                 }
-                                if (L > N || s > 1 && L == N && !h && Math.random() < .5) {
+                                if (L > N || ply > 1 && L == N && !h && Math.random() < .5) {
                                     I[G] = O;
-                                    if (s > 1) {
+                                    if (ply > 1) {
                                         if (h && c - L < 0)
                                             return y ^= 8,
                                             G--,
                                             L;
                                         if (!h)
                                             i = n,
-                                            B = O,
-                                            b = p;
+                                            selectedCellNumber = O,
+                                            userSelectedCellNumber = cellNumber;
                                     }
                                     N = L;
                                 }
-                                n += J || (g = p,
-                                m = p < O ? g - 3 : g + 2,
-                                I[m] < z | I[m + O - p] || I[p += p - O]) ? 1 : 0;
+                                n += J || (g = cellNumber,
+                                m = cellNumber < O ? g - 3 : g + 2,
+                                I[m] < z | I[m + O - cellNumber] || I[cellNumber += cellNumber - O]) ? 1 : 0;
                             }
                         }
                     }
-                } while (!r & q > 2 || (p = O,
+                } while (!r & q > 2 || (cellNumber = O,
                 q | A > 2 | o > z & !r && ++C * --A));
             }
         }
@@ -73,41 +73,41 @@ const X = (w, c, h, e, S, s) => {
     N + M * M && N > -K + 1924 | d ? N : 0;
 }
 
-const W = () => {
-    B = b;
-    for (p = 21; p < 99; ++p)
-        if (q = document.getElementById("o" + p)) {
-            q.innerHTML = "\xa0\u265f\u265a\u265e\u265d\u265c\u265b  \u2659\u2654\u2658\u2657\u2656\u2655".charAt(I[p] & z);
-            q.style.borderColor = p == B ? "firebrick" : "#dde";
+const drawBoard = () => {
+    selectedCellNumber = userSelectedCellNumber;
+    for (cellNumber = 21; cellNumber < 99; ++cellNumber)
+        if (q = document.getElementById("position" + cellNumber)) {
+            q.innerHTML = "\xa0\u265f\u265a\u265e\u265d\u265c\u265b  \u2659\u2654\u2658\u2657\u2656\u2655".charAt(I[cellNumber] & z);
+            q.style.borderColor = cellNumber == selectedCellNumber ? "firebrick" : "#dde";
         }
 }
 
 window.onload = () => {
-    B = i = y = u = 0;
-    while (B++ < 120)
-        I[B - 1] = B % x ? B / x % x < 2 | B % x < 2 ? 7 : B / x & 4 ? 0 : l[i++] | 16 : 7;
+    selectedCellNumber = i = y = u = 0;
+    while (selectedCellNumber++ < 120)
+        I[selectedCellNumber - 1] = selectedCellNumber % x ? selectedCellNumber / x % x < 2 | selectedCellNumber % x < 2 ? 7 : selectedCellNumber / x & 4 ? 0 : l[i++] | 16 : 7;
     for (a = "<table cellspacing=0 align=center style='border:5px solid #dde;border-radius:5px' bgcolor=#dde>",
-    i = 18; i < 100; a += ++i % 10 - 9 ? "<th width=80 height=80 onclick=computerNextMove(" + i + ") id=o" + i + " style='line-height:50px;font-size:50px;border:2px solid #dde' bgcolor=#" + (i * .9 & 1 ? "c0c0f0" : "e6e6fa") + ">" : (i++,
+    i = 18; i < 100; a += ++i % 10 - 9 ? "<th width=80 height=80 onclick=computerNextMove(" + i + ") id=position" + i + " style='line-height:50px;font-size:50px;border:2px solid #dde' bgcolor=#" + (i * .9 & 1 ? "c0c0f0" : "e6e6fa") + ">" : (i++,
     "<tr>"))
         ;
     a += "<th colspan=8><select id=t style='font-size:20px'><option>&#9819;<option>";
     document.getElementById("board").innerHTML = a + "&#9820;<option>&#9821;<option>&#9822;</select></table>";
 
-    W();
+    drawBoard();
 }
 
 const computerNextMove = (s) => {
     i = (I[s] ^ y) & z;
     if (i > 8) {
-        b = s;
-        W();
-    } else if (B && i < 9) {
-        b = s;
-        i = I[B] & z;
-        if ((i & 7) == 1 & (b < 29 | b > 90))
+        userSelectedCellNumber = s;
+        drawBoard();
+    } else if (selectedCellNumber && i < 9) {
+        userSelectedCellNumber = s;
+        i = I[selectedCellNumber] & z;
+        if ((i & 7) == 1 & (userSelectedCellNumber < 29 | userSelectedCellNumber > 90))
             i = 14 - document.getElementById("t").selectedIndex ^ y;
-        X(0, 0, 0, 21, u, 1);
+        calculateNextMove(0, 0, 0, 21, u, 1);
         if (y)
-            setTimeout("X(0,0,0,21,u,2/*ply*/),X(0,0,0,21,u,1)", 250);
+            setTimeout("calculateNextMove(0,0,0,21,u,2/*ply*/),calculateNextMove(0,0,0,21,u,1)", 250);
     }
 }
